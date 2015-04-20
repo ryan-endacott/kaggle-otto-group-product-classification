7ccf8dc3af27cfefd5416a105825e51e3e7bdbcc
Ryan Endacott <rzeg24@gmail.com>
2015-04-20 14:47:34 -0500

    Write submission function.

--- data.py
+++ data.py
@@ -13,6 +13,7 @@ kaggle_train_features = kaggle_train_features.drop('target', 1)
 kaggle_train_features = kaggle_train_features.drop('id', 1)
 
 test_features = test_data.drop('id', 1)
+test_ids = test_data['id']
 
 kaggle_train_targets = kaggle_train_data['target']
 
@@ -36,6 +37,19 @@ def print_accuracy(predictions, targets):
     percent_correct = (float(num_correct) / total) * 100
     print("Got {0}% accuracy with {1} correct predictions out of {2}."
             .format(percent_correct, num_correct, total))
+    return
+
+def write_submission(ids, predictions, filename):
+    print("Writing submission to {0}...".format(filename))
+    with open('submissions/' + filename, 'w') as f:
+        f.write("id,Class_1,Class_2,Class_3,Class_4,Class_5,Class_6,Class_7" +
+            ",Class_8,Class_9\n")
+        for idx, prediction in enumerate(predictions):
+            results = [0] * 9
+            prediction_int = int(prediction[-1])
+            results[prediction_int - 1] = 1
+            f.write("{0},{1}\n"
+                    .format(ids[idx], ','.join(str(res) for res in results)))
 
 # Reseed random so it's not based on the same seed anymore.
 np.random.seed()
