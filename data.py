5a4b3745401ba67b4de12ae0178699675986c143
Ryan Endacott <rzeg24@gmail.com>
2015-04-20 14:14:48 -0500

    Get random subset for train and validation datasets.

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 # Loads the data.
 
 import pandas as pd
+import numpy as np
 
 kaggle_train_data = pd.read_csv('train.csv')
 
@@ -17,11 +18,14 @@ kaggle_train_targets = kaggle_train_data['target']
 
 # Split kaggle training data into test and validation with a split of
 # 80% test, 20% validation.
-split_index = int(len(kaggle_train_features) * .8)
-train_features = kaggle_train_features[:split_index]
-validation_features = kaggle_train_features[split_index:]
-train_targets = kaggle_train_targets[:split_index]
-validation_targets = kaggle_train_targets[split_index:]
+np.random.seed(1337)
+split_value = int(len(kaggle_train_features) * .8)
+rows = np.random.choice(kaggle_train_features.index.values, split_value,
+        replace=False)
+train_features = kaggle_train_features.ix[rows]
+validation_features = kaggle_train_features.drop(rows)
+train_targets = kaggle_train_targets.ix[rows]
+validation_targets = kaggle_train_targets.drop(rows)
 
 def print_accuracy(predictions, targets):
     total = len(predictions)
@@ -33,3 +37,5 @@ def print_accuracy(predictions, targets):
     print("Got {0}% accuracy with {1} correct predictions out of {2}."
             .format(percent_correct, num_correct, total))
 
+# Reseed random so it's not based on the same seed anymore.
+np.random.seed()
