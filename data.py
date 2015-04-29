e478e96954b98569dff393b87f8a679d09fffd73
Ryan Endacott <rzeg24@gmail.com>
2015-04-28 23:36:49 -0500

    Cross validation training and predict_proba naive bayes.

--- data.py
+++ data.py
@@ -2,54 +2,37 @@
 
 import pandas as pd
 import numpy as np
+import scipy as sp
 
-kaggle_train_data = pd.read_csv('train.csv')
+train_data = pd.read_csv('train.csv')
 
 test_data = pd.read_csv('test.csv')
 
 # Set up the features and targets.
-kaggle_train_features = kaggle_train_data
-kaggle_train_features = kaggle_train_features.drop('target', 1)
-kaggle_train_features = kaggle_train_features.drop('id', 1)
+train_features = train_data
+train_features = train_features.drop('target', 1)
+train_features = train_features.drop('id', 1)
 
 test_features = test_data.drop('id', 1)
 test_ids = test_data['id']
 
-kaggle_train_targets = kaggle_train_data['target']
+train_targets = train_data['target']
 
-# Split kaggle training data into test and validation with a split of
-# 80% test, 20% validation.
-np.random.seed(1337)
-split_value = int(len(kaggle_train_features) * .8)
-rows = np.random.choice(kaggle_train_features.index.values, split_value,
-        replace=False)
-train_features = kaggle_train_features.ix[rows]
-validation_features = kaggle_train_features.drop(rows)
-train_targets = kaggle_train_targets.ix[rows]
-validation_targets = kaggle_train_targets.drop(rows)
-
-def print_accuracy(predictions, targets):
-    total = len(predictions)
-    if total != len(targets):
-        print "Error: Prediction and target dimensions don't match."
-        return
-    num_correct = (predictions == targets).sum()
-    percent_correct = (float(num_correct) / total) * 100
-    print("Got {0}% accuracy with {1} correct predictions out of {2}."
-            .format(percent_correct, num_correct, total))
-    return
+def print_validation_score(scores):
+    print("Cross validation logloss score: {0} +- {1}"
+            .format(-scores.mean(), scores.std() * 2))
 
 def write_submission(ids, predictions, filename):
-    print("Writing submission to {0}...".format(filename))
+    print("Writing submission to submissions/{0}...".format(filename))
+
+    # Build the whole file contents to be written for efficiency.
+    pred_str_arr = []
+    for idx, prediction in enumerate(predictions):
+        pred_str_arr.append(
+                "%d,%s\n" % (ids[idx], ','.join(str(p) for p in prediction)))
+
     with open('submissions/' + filename, 'w') as f:
         f.write("id,Class_1,Class_2,Class_3,Class_4,Class_5,Class_6,Class_7" +
-            ",Class_8,Class_9\n")
-        for idx, prediction in enumerate(predictions):
-            results = [0] * 9
-            prediction_int = int(prediction[-1])
-            results[prediction_int - 1] = 1
-            f.write("{0},{1}\n"
-                    .format(ids[idx], ','.join(str(res) for res in results)))
-
-# Reseed random so it's not based on the same seed anymore.
-np.random.seed()
+                ",Class_8,Class_9\n")
+        f.write(''.join(pred_str_arr))
+    print("Done writing submission.")
--- naivebayes.py
+++ naivebayes.py
@@ -1,15 +1,19 @@
 # Performs Naive Bayes on the data set.
 
 from data import *
+from sklearn import cross_validation
 from sklearn.naive_bayes import GaussianNB
 
 print("Running Naive Bayes...")
+
+gnb = GaussianNB()
+scores = cross_validation.cross_val_score(gnb, train_features, train_targets,
+    cv = 10, scoring='log_loss')
+print_validation_score(scores)
+
+print("Predicting test data...")
 gnb = GaussianNB()
 gnb.fit(train_features, train_targets)
-validation_predictions = gnb.predict(validation_features)
+test_predictions = gnb.predict_proba(test_features)
 
-print_accuracy(validation_predictions, validation_targets)
-
-test_predictions = gnb.predict(test_features)
-
-write_submission(test_data['id'], test_predictions, 'naivebayes.csv')
+write_submission(test_ids, test_predictions, 'naivebayes.csv')
