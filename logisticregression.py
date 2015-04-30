d1fca58d7882808bb72fe0632c6a317efb68ee53
Ryan Endacott <rzeg24@gmail.com>
2015-04-30 01:33:59 -0500

    logistic regression.

--- logisticregression.py
+++ logisticregression.py
@@ -1,13 +1,13 @@
-# Performs Naive Bayes on the data set.
+# Performs Logistic Regression on the data set.
 
 from data import *
 from sklearn import cross_validation
-from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
-print("Running GaussianNB...")
+print("Running Logistic Regression...")
 
 print("Performing 10-fold cross validation...")
-clf = GaussianNB()
+clf = LogisticRegression()
 scores = cross_validation.cross_val_score(clf, train_features, train_targets,
     cv = 10, scoring='log_loss')
 print_validation_score(scores)
@@ -16,4 +16,4 @@ print("Predicting test data...")
 clf.fit(train_features, train_targets)
 test_predictions = clf.predict_proba(test_features)
 
-write_submission(test_ids, test_predictions, 'naivebayes.csv')
+write_submission(test_ids, test_predictions, 'logisticregression.csv')
