4e7d4e7ccb57f8eb895127e34f051b28eb0571bc
Ryan Endacott <rzeg24@gmail.com>
2015-05-13 01:30:07 -0500

    plot pca

--- pca.py
+++ pca.py
@@ -2,15 +2,19 @@
 
 from data import *
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 # Note: Total number of features is 93.
 pca = PCA(n_components = 93)
 pca.fit(train_features)
 variance_ratio = pca.explained_variance_ratio_
 
+def get_explained_variance(n_components):
+    return sum(variance_ratio[0:n_components])
+
 def print_pca_explained_variance(n_components):
    print("The variance explained by {0} components is {1}.\n"
-           .format(n_components, sum(variance_ratio[0:n_components])))
+           .format(n_components, get_explained_variance(n_components)))
 
 print_pca_explained_variance(93)
 print_pca_explained_variance(92)
@@ -20,5 +24,14 @@ print_pca_explained_variance(30)
 print_pca_explained_variance(10)
 print_pca_explained_variance(5)
 
+
+X = range(0, 93)
+Y = [get_explained_variance(x) for x in X]
+
+plt.plot(X, Y)
+plt.axis([0, 93, 0, 1])
+plt.ylabel('Proportion of Variance (beta k)')
+plt.xlabel('Number of Features')
+plt.show()
 
 
